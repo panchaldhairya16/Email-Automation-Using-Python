e207c648a3f1d8449e4cce713e3b20f7b458dc0b
Dhairya Panchal <panchaldhairya6464@gmail.com>
2026-08-19 15:01:42 +0530

    Update email address placeholder in send_email.py

--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-email_user = "YOUR_MAIL_ADDR"
+email_user = "YOUR_EMAIL_ADDRESS"
 password = "YOUR_PASSWORD"
 subject = "Holiday Reminder"
 
